04d17caf4e3b81e0f2cf4a00a0ac4c5bd0c2a331
Amul R <amul.r2024@teachforindia.org>
2025-06-06 15:27:46 +0530

    Ensuring build/web is properly tracked and pushed

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "239944715"
+    serviceWorkerVersion: "1384482573"
   }
 });
